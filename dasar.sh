02f630919715d9e8ca0c4c151cbbe4c16efd0274
RouzanSantriCyber21 <56404657+RouzanSantriCyber21@users.noreply.github.com>
2019-12-06 10:16:30 +0700

    Create dasar.sh
    Dasar v1.0

---
+++ dasar.sh
@@ -0,0 +1,18 @@
+clear
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pkg install gem
+pkg install ruby
+gem install lolcat
+pkg install figlet
+pkg install toilet
+pkg install neofetch
+pkg install screenfetch
+pkg install openssh
+pkg install vim
+pkg install git
+pkg install nano
+pkg install php
+
